60750df86944f8cc7006e66fb4cefb7a75c19be4
tprototype <kishmat17@gmail.com>
2023-01-05 19:41:58 +0545

    auth-and-dbmigrate

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 """
 Flask creates the instance of WSGI application
 """
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
+migrate = Migrate(app, db, render_as_batch=True)
+
+app.secret_key = "j54dsjdsd)021-jdsfjdkfddf?,/l.df"
 
 
-from models import User
+from models import User, Post
 
 
 @app.route("/")
 def index():
-    users = User.query.all()
-    for user in users:
-        print(user.username)
     """This view function is for the home page"""
+    # users = User.query.all()
+    # for user in users:
+    #     print(user.username)
     return render_template("index.html")
 
 
@@ -28,8 +32,19 @@ def topic(cat):
     return render_template("topic.html", cat=cat)
 
 
-@app.route("/submit")
+@app.route("/submit", methods=["GET", "POST"])
 def submit():
+    if "username" not in session:
+        return redirect(url_for("login"))
+    if request.method == "POST":
+        author = User.query.filter_by(username=session["username"]).first()
+        title = request.form["title"]
+        content = request.form["content"]
+        new_post = Post(title=title, content=content, author=author)
+        db.session.add(new_post)
+        db.session.commit()
+        print("Post Added")
+        return redirect(url_for("index"))
     """This view function is for the submit page"""
     return render_template("submit.html")
 
@@ -43,6 +58,7 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             # login successful
+            session["username"] = username
             return redirect(url_for("index"))
         else:
             # login failed
@@ -52,7 +68,7 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """dkdk"""
+    """function to create an account"""
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -60,8 +76,18 @@ def register():
         new_user = User(username=username, password=password_hash)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
     return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    if "username" not in session:
+        return redirect(url_for("login"))
+    """function to logout the user by
+    removing the user from the session"""
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
 
 # html escaping
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 """Models.py"""
+from datetime import datetime
 from app import db
 
 
@@ -8,3 +9,30 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+    comments = db.relationship("Comment", backref="author", lazy=True)
+
+
+class Post(db.Model):
+    """Post Model with necessary fields"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    source = db.Column(db.String, nullable=True)
+    upvotes = db.Column(db.Integer, default=0)
+    category = db.Column(db.String, nullable=True)
+    comments = db.relationship("Comment", backref="post", lazy=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+
+class Comment(db.Model):
+    """Comment Model with necessary fields"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+    upvotes = db.Column(db.Integer, default=0)
+    author_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_at = db.Column(db.DateTime, default=datetime.now)
